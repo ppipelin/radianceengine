c4f001a32b68233ca75d2d5a186f4e5818496a8a
Paul-Elie Pipelin <paul.elie@free.fr>
2023-12-28 10:40:44 +0100

    Support UCI, remove other UI and cleaned

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,12 @@
 #include "include.h"
-#include "board.h"
-#include "boardParser.h"
-// #include "connect.h"
-#include "client.h"
-#include "cMove.h"
-
-#include "annex.h"
+#include "uci.h"
 
 #include <iostream>
-#include <chrono>
 
-int main()
+int main(int argc, char *argv[])
 {
-	Game game = Game();
-	// Client client = Client(game);
-	// client.connect();
-
-	// Classic Perft Results
-	// 4 -       197 281
-	// 5 -     4 865 609 with en passant + castle
-	// 6 -   119 060 324 - 1 207 806ms
-
-	game.m_boardParser->fillBoard("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-	// Kiwipete position
-	// 1 -	           48 -- Result: 48
-	// 2	-         2 039         2 039
-	// 3	-        97 862        97 862 // 1 425ms
-	// 4 -     4 085 603     4 085 603 // 54s
-	// 5 -   193 690 690
-	// 6 - 8 031 647 685
-
-	// game.m_boardParser->fillBoard("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"); // Pos 3 - 6 passed
-	// game.m_boardParser->fillBoard("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"); // Pos 4 - 5 passed 202s
-
-	// game.m_boardParser->fillBoard("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"); // Pos 5
-	// Position 5
-	// 1 -           	44 -- OK
-	// 2 - 	       1 486 -- OK
-	// 3 - 	      62 379 -- OK
-	// 4 -    	2 103 487 -- OK
-	// 5	-    89 941 194 -- Result: ?
-
-	// game.m_boardParser->fillBoard("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"); // Pos 6 - 4 passed (5 is 164 075 514 instead of 164 075 551)
-
-	// game.m_boardParser->displayCLI();
-	// std::cout << std::endl;
-	// game.m_boardParser->displayCout();
-	// std::cout << std::endl;
-	// for (UInt i = 0; i < BOARD_SIZE2; ++i)
-	// {
-	// 	game.m_boardParser->displayCLIMove(i);
-	// 	std::cout << std::endl;
-	// }
-	// game.m_boardParser->displayCLIWhiteBlack();
-
-	int perft_value = 3;
-	auto t1 = std::chrono::high_resolution_clock::now();
-	std::cout << "perft(" << perft_value << ") : " << perft(*game.m_boardParser, perft_value, true) << std::endl;
-	auto t2 = std::chrono::high_resolution_clock::now();
-	auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-	std::cout << "Total time for perft: " << ms_int.count() << "ms" << std::endl;
-
-	// demo();
-
+	UCI::init(Options);
+	UCI::loop(argc, argv);
 	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
---
+++ src/uci.cpp
@@ -0,0 +1,235 @@
+#include "uci.h"
+
+#include "cMove.h"
+#include "boardParser.h"
+#include "search.h"
+#include "searchRandom.h"
+#include "evaluate.h"
+#include "evaluateShannon.h"
+
+#include <queue>
+
+namespace {
+	const std::string startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+	void position(BoardParser &pos, std::istringstream &is)
+	{
+		cMove c;
+		std::string token, fen;
+
+		is >> token;
+		if (token == "startpos")
+		{
+			fen = startFen;
+			is >> token; // Consume the "moves" token, if any
+		}
+		else if (token == "fen")
+		{
+			while (is >> token && token != "moves")
+				fen += token + " ";
+		}
+		else
+			return;
+
+		pos.fillBoard(fen);
+
+		// Parse the moves list, if any
+		while (is >> token && (c = UCI::to_move(pos, token)) != cMove())
+		{
+			pos.movePiece(c);
+		}
+	}
+
+	// setoption() is called when the engine receives the "setoption" UCI command.
+	// The function updates the UCI option ("name") to the given value ("value").
+	void setoption(std::istringstream &is)
+	{
+		std::string token, name, value;
+
+		is >> token; // Consume the "name" token
+
+		// Read the option name (can contain spaces)
+		while (is >> token && token != "value")
+			name += (name.empty() ? "" : " ") + token;
+
+		// Read the option value (can contain spaces)
+		while (is >> token)
+			value += (value.empty() ? "" : " ") + token;
+
+		if (Options.count(name))
+			Options[name] = value;
+		else
+			std::cout << "No such option: " << name << std::endl;
+	}
+
+	// go() is called when the engine receives the "go" UCI command. The function
+	// sets the thinking time and other parameters from the input string, then starts
+	// with a search.
+	void go(const BoardParser &pos, std::istringstream &is)
+	{
+		SearchData::LimitsType limits;
+		std::string token;
+
+		// limits.startTime = now(); // The search starts as early as possible
+
+		while (is >> token)
+		{
+			if (token == "searchmoves") // Needs to be the last command on the line
+				while (is >> token)
+					// limits.searchmoves.push_back(UCI::to_move(pos, token));
+					;
+			// else if (token == "wtime")     is >> limits.time[WHITE];
+			// else if (token == "btime")     is >> limits.time[BLACK];
+			// else if (token == "winc")      is >> limits.inc[WHITE];
+			// else if (token == "binc")      is >> limits.inc[BLACK];
+			// else if (token == "movestogo") is >> limits.movestogo;
+			else if (token == "depth")     is >> limits.depth;
+			// else if (token == "nodes")     is >> limits.nodes;
+			// else if (token == "movetime")  is >> limits.movetime;
+			// else if (token == "mate")      is >> limits.mate;
+			else if (token == "perft")     is >> limits.perft;
+			// else if (token == "infinite")  limits.infinite = 1;
+			// else if (token == "ponder")    ponderMode = true;
+		}
+		SearchRandom search = SearchRandom();
+		EvaluateShannon evaluate = EvaluateShannon();
+		cMove move = search.nextMove(pos, evaluate);
+
+		std::cout << "bestmove " << UCI::move(move) << std::endl;
+	}
+}
+
+/// UCI::loop() waits for a command from the stdin, parses it and then calls the appropriate
+/// function. It also intercepts an end-of-file (EOF) indication from the stdin to ensure a
+/// graceful exit if the GUI dies unexpectedly. When called with some command-line arguments,
+/// like running 'bench', the function returns immediately after the command is executed.
+/// In addition to the UCI ones, some additional debug commands are also supported.
+void UCI::loop(int argc, char *argv[])
+{
+	BoardParser pos = BoardParser();
+	std::string token, cmd;
+
+	pos.fillBoard(startFen);
+
+	std::queue<std::string> q;
+	for (int i = 1; i < argc; ++i)
+		q.push(std::string(argv[i]));
+	do
+	{
+		do // do-while until queue is empty
+		{
+			if (!q.empty())
+			{
+				cmd = q.front();
+				q.pop();
+			}
+			if (argc == 1 && !getline(std::cin, cmd)) // Wait for an input or an end-of-file (EOF) indication
+				cmd = "quit";
+
+			std::istringstream is(cmd);
+
+			token.clear(); // Avoid a stale if getline() returns nothing or a blank line
+			is >> std::skipws >> token;
+
+			if (token == "quit" || token == "stop")
+				std::cout << "UCI - quitting..." << std::endl;
+
+			// The GUI sends 'ponderhit' to tell that the user has played the expected move.
+			// So, 'ponderhit' is sent if pondering was done on the same move that the user
+			// has played. The search should continue, but should also switch from pondering
+			// to the normal search.
+			else if (token == "ponderhit")
+				std::cout << "UCI - ponderhit received" << std::endl;
+			else if (token == "uci")
+				std::cout << "id name chessengine" << "\n" << Options << "\nuciok" << std::endl;
+			else if (token == "setoption")
+				setoption(is);
+			else if (token == "go")
+				go(pos, is);
+			else if (token == "position")
+				position(pos, is);
+			else if (token == "isready")
+				std::cout << "readyok" << std::endl;
+			else if (token == "eval")
+				std::cout << "UCI - eval called" << std::endl;
+			else if (token == "--help" || token == "help" || token == "--license" || token == "license")
+				std::cout << "\nRadiance is chess engine for playing and analyzing."
+				"\nIt supports Universal Chess Interface (UCI) protocol to communicate with a GUI, an API, etc."
+				"\nor read the corresponding README.md and Copying.txt files distributed along with this program." << std::endl;
+			else if (!token.empty() && token[0] != '#')
+				std::cout << "UCI - Unknown command: '" << cmd << "'." << std::endl;
+		} while (!q.empty());
+	} while (token != "quit" && argc == 1); // The command-line arguments are one-shot
+}
+
+/// UCI::square() converts a const UInt to a string in algebraic notation (g1, a7, etc.)
+std::string UCI::square(const UInt s)
+{
+	return Board::toString(s);
+}
+
+/// UCI::move() converts a Move to a string in coordinate notation (g1f3, a7a8q).
+std::string UCI::move(cMove m)
+{
+	if (m == MOVE_NONE)
+		return "(none)";
+
+	if (m == MOVE_NULL)
+		return "0000";
+
+	const UInt from = m.getFrom();
+	const UInt to = m.getTo();
+
+	std::string move = Board::toString(from) + Board::toString(to);
+
+	if (m.isPromotion())
+		move += "nbrq"[m.getFlags() << 2 >> 2]; // keep last two bits
+
+	return move;
+}
+
+/// UCI::to_move() converts a string representing a move in coordinate notation
+/// (g1f3, a7a8q) to the corresponding legal Move, if any.
+cMove UCI::to_move(const BoardParser &pos, std::string &str)
+{
+	UInt flags = 0;
+	UInt from = Board::toTiles(str.substr(0, 2));
+	UInt to = Board::toTiles(str.substr(2, 2));
+	// Capture flag
+	if ((*pos.boardParsed())[to] != nullptr)
+		flags &= 0x4;
+	if (str.length() == 5)
+	{
+		// The promotion piece character must be lowercased
+		str[4] = char(tolower(str[4]));
+		flags &= 0x8;
+		switch (str[4])
+		{
+		case 'b':
+			flags &= 0x1;
+			break;
+		case 'r':
+			flags &= 0x2;
+			break;
+		case 'q':
+			flags &= 0x3;
+			break;
+		}
+	}
+
+	const Piece *p = (*pos.boardParsed())[from];
+	// Detect castle and flag
+	if (p != nullptr && typeid(*p) == typeid(King))
+	{
+		if (to - from == 2)
+		{
+			flags = 0x2;
+		}
+		else if (from - to == 2)
+		{
+			flags = 0x3;
+		}
+	}
+
+	return cMove(from, to, flags);
+}
