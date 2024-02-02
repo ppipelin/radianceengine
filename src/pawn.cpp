ec8a327cefd8d555ffbd97bfc3a48f378679bb3b
Paul-Elie Pipelin <paul.elie97@gmail.com>
2024-02-02 15:43:03 +0100

    Fixed pawn promote - using pointers, couldn't find the way to make it work with references

--- src/pawn.cpp
+++ src/pawn.cpp
@@ -2,8 +2,23 @@
 
 void Pawn::canMove(const Board &b, std::vector<cMove> &v) const
 {
-	v.reserve(v.size() + 4);
 	const UInt c = Board::column(m_tile);
+	// Default behavior, we push_back() to v
+	std::vector<cMove> tmp = std::vector<cMove>();
+	std::vector<cMove> *vRef = &tmp;
+	// Promotion cases uses temporary vector to duplicate moveset during promotion
+	const bool promotion = m_isWhite && Board::row(m_tile + BOARD_SIZE) == BOARD_SIZE - 1 || !m_isWhite && Board::row(m_tile - BOARD_SIZE) == 0;
+	if (promotion)
+	{
+		vRef->reserve(4);
+		v.reserve(v.size() + 4 * 4);
+	}
+	else
+	{
+		vRef = &v;
+		vRef->reserve(vRef->size() + 4);
+	}
+
 	if (m_isWhite)
 	{
 		// Can move forward
@@ -13,27 +28,27 @@ void Pawn::canMove(const Board &b, std::vector<cMove> &v) const
 			// Forward and double forward
 			if (b[forward] == nullptr)
 			{
-				v.push_back(cMove(m_tile, forward));
+				vRef->push_back(cMove(m_tile, forward));
 				if (Board::row(m_tile) == 1 && b[forward + BOARD_SIZE] == nullptr)
-					v.push_back(cMove(m_tile, forward + BOARD_SIZE));
+					vRef->push_back(cMove(m_tile, forward + BOARD_SIZE));
 			}
 			// Forward left (checks white + not on first column)
 			if (!Board::leftCol(m_tile) && b[forward - 1] != nullptr && !b[forward - 1]->isWhite())
-				v.push_back(cMove(m_tile, forward - 1, 4));
+				vRef->push_back(cMove(m_tile, forward - 1, 4));
 			// Forward right (checks white + not on last column)
 			if (!Board::rightCol(m_tile) && b[forward + 1] != nullptr && !b[forward + 1]->isWhite())
-				v.push_back(cMove(m_tile, forward + 1, 4));
+				vRef->push_back(cMove(m_tile, forward + 1, 4));
 			// Adding en passant
 			Int enPassantCol = b.enPassant();
 			if (Board::row(m_tile) == 4 && enPassantCol != -1)
 			{
 				if (enPassantCol == Int(c + 1))
 				{
-					v.push_back(cMove(m_tile, m_tile + BOARD_SIZE + 1, 5));
+					vRef->push_back(cMove(m_tile, m_tile + BOARD_SIZE + 1, 5));
 				}
 				else if (enPassantCol == Int(c - 1))
 				{
-					v.push_back(cMove(m_tile, m_tile + BOARD_SIZE - 1, 5));
+					vRef->push_back(cMove(m_tile, m_tile + BOARD_SIZE - 1, 5));
 				}
 			}
 		}
@@ -47,27 +62,27 @@ void Pawn::canMove(const Board &b, std::vector<cMove> &v) const
 			// Forward and double forward
 			if (b[forward] == nullptr)
 			{
-				v.push_back(cMove(m_tile, forward));
+				vRef->push_back(cMove(m_tile, forward));
 				if (Board::row(m_tile) == BOARD_SIZE - 2 && b[forward - BOARD_SIZE] == nullptr)
-					v.push_back(cMove(m_tile, forward - BOARD_SIZE));
+					vRef->push_back(cMove(m_tile, forward - BOARD_SIZE));
 			}
 			// Forward left (checks black + not on first column)
 			if (!Board::leftCol(m_tile) && b[forward - 1] != nullptr && b[forward - 1]->isWhite())
-				v.push_back(cMove(m_tile, forward - 1, 4));
+				vRef->push_back(cMove(m_tile, forward - 1, 4));
 			// One black forward right (checks black + not on last column)
 			if (!Board::rightCol(m_tile) && b[forward + 1] != nullptr && b[forward + 1]->isWhite())
-				v.push_back(cMove(m_tile, forward + 1, 4));
+				vRef->push_back(cMove(m_tile, forward + 1, 4));
 			// Adding en passant
 			Int enPassantCol = b.enPassant();
 			if (Board::row(m_tile) == 3 && enPassantCol != -1)
 			{
 				if (enPassantCol == Int(c + 1))
 				{
-					v.push_back(cMove(m_tile, m_tile - BOARD_SIZE + 1, 5));
+					vRef->push_back(cMove(m_tile, m_tile - BOARD_SIZE + 1, 5));
 				}
 				else if (enPassantCol == Int(c - 1))
 				{
-					v.push_back(cMove(m_tile, m_tile - BOARD_SIZE - 1, 5));
+					vRef->push_back(cMove(m_tile, m_tile - BOARD_SIZE - 1, 5));
 				}
 			}
 		}
@@ -75,9 +90,7 @@ void Pawn::canMove(const Board &b, std::vector<cMove> &v) const
 	// If we are going to the last rank, previous computed moves are promotions
 	if (m_isWhite && Board::row(m_tile + BOARD_SIZE) == BOARD_SIZE - 1 || !m_isWhite && Board::row(m_tile - BOARD_SIZE) == 0)
 	{
-		std::vector<cMove> v_tmp = std::move(v);
-		v.reserve(v_tmp.capacity() * 4);
-		for (auto &move : v_tmp)
+		for (auto &move : *vRef)
 		{
 			if (move.isCapture())
 			{
