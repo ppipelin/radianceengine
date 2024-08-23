115df962f9e1cf2b8bd8232df9e1fb01fa710532
Paul-Elie Pipelin <paul.elie97@gmail.com>
2024-08-23 09:35:58 +0200

    Add BB gen for queen

--- src/queen.cpp
+++ src/queen.cpp
@@ -1,12 +1,35 @@
 #include "queen.h"
 
-void Queen::canMove(const Board &b, std::vector<cMove> &v) const
+void Queen::canMove(const Board &, std::vector<cMove> &v) const
 {
 	v.reserve(v.size() + 27);
-	constexpr std::array<Int, 8> directions{ 1, -1, 8, -8, 9, -9, 7, -7 };
-	for (const Int direction : directions)
+	const Bitboard blockersRook = Bitboards::movesRookMask[m_tile] & Bitboards::bbPieces[PieceType::ALL];
+	Bitboard pseudoLegalMovesBBPartialRook = 0;
+	if (blockersRook == 0)
+		pseudoLegalMovesBBPartialRook = Bitboards::movesRook[m_tile];
+	else
+		pseudoLegalMovesBBPartialRook = Bitboards::movesRookLegal[m_tile][blockersRook];
+
+	const Bitboard blockersBishop = Bitboards::movesBishopMask[m_tile] & Bitboards::bbPieces[PieceType::ALL];
+	Bitboard pseudoLegalMovesBBPartialBishop = 0;
+	if (blockersBishop == 0)
+		pseudoLegalMovesBBPartialBishop = Bitboards::movesBishop[m_tile];
+	else
+		pseudoLegalMovesBBPartialBishop = Bitboards::movesBishopLegal[m_tile][blockersBishop];
+
+	Bitboard pseudoLegalQuietBB = (pseudoLegalMovesBBPartialRook | pseudoLegalMovesBBPartialBishop) & ~Bitboards::bbPieces[PieceType::ALL];
+	Bitboard pseudoLegalCapturesBB = (pseudoLegalMovesBBPartialRook | pseudoLegalMovesBBPartialBishop) & Bitboards::allPiecesColor(Color(!isWhite()));
+
+	// Quiet moves
+	while (pseudoLegalQuietBB)
 	{
-		sliding(b, direction, v);
+		v.push_back(cMove(m_tile, Bitboards::popLeastBit(pseudoLegalQuietBB), 0));
+	}
+
+	// Capture moves
+	while (pseudoLegalCapturesBB)
+	{
+		v.push_back(cMove(m_tile, Bitboards::popLeastBit(pseudoLegalCapturesBB), 4));
 	}
 }
 
