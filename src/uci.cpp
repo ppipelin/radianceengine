005b87b27e86db289a18f62880ac4045d0d358e1
ppipelin <paul.elie@free.fr>
2024-12-23 17:32:07 +0100

    Fixed nps display

--- src/uci.cpp
+++ src/uci.cpp
@@ -322,7 +322,7 @@ std::string UCI::pv(const Search &s, UInt depth)
 		ss << "info"
 			<< " depth " << depth
 			<< " nodes " << nodes
-			<< " nps " << nodes * 1000 / std::max(s.elapsed(), TimePoint(1))
+			<< " nps " << UIntL(nodes) * 1000 / std::max(s.elapsed(), TimePoint(1))
 			<< " hash " << transpositionTable.size()
 			<< " hashfull " << std::round(transpositionTable.size() * 1000 / transpositionTable.max_size())
 			<< " hashused " << s.transpositionUsed
